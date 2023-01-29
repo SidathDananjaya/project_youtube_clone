f2db4effe8e205e197b93a30a0871647f681343c
SidathDananjaya <62599004+SidathDananjaya@users.noreply.github.com>
2023-01-30 00:18:50 +0530

    Add files via upload

---
+++ src/components/SearchFeed.jsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react'
+import { useParams } from "react-router-dom";
+import { Box, Stack, Typography } from '@mui/material'
+import { Sidebar, Videos } from './'
+import { fetchFromAPI } from '../utils/fetchFromAPI'
+
+const SearchFeed = () => {
+    const [videos, setVideos] = useState(null);
+    const { searchTerm } = useParams();
+
+    useEffect(() => {
+        fetchFromAPI(`search?part=snippet&q=${searchTerm}`)
+            .then((data) => setVideos(data.items))
+    }, []);
+
+    return (
+        <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+            <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+                Search Result for: <span style={{ color: "#FC1503" }}>{searchTerm}</span>videos
+            </Typography>
+
+            <Videos videos={videos} />
+        </Box>
+    )
+}
+
+export default SearchFeed
---
+++ src/utils/constants.js
@@ -0,0 +1,43 @@
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import HomeIcon from '@mui/icons-material/Home';
+import CodeIcon from '@mui/icons-material/Code';
+import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+import SchoolIcon from '@mui/icons-material/School';
+import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural';
+import CheckroomIcon from '@mui/icons-material/Checkroom';
+import GraphicEqIcon from '@mui/icons-material/GraphicEq';
+import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
+
+export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
+
+export const categories = [
+    { name: 'New', icon: <HomeIcon />, },
+    { name: 'JS Mastery', icon: <CodeIcon />, },
+    { name: 'Ever Show Gaming', icon: <SportsEsportsIcon />, },
+    { name: 'Coding', icon: <CodeIcon />, },
+    { name: 'ReactJS', icon: <CodeIcon />, },
+    { name: 'NextJS', icon: <CodeIcon />, },
+    { name: 'Music', icon: <MusicNoteIcon /> },
+    { name: 'Education', icon: <SchoolIcon />, },
+    { name: 'Podcast', icon: <GraphicEqIcon />, },
+    { name: 'Movie', icon: <OndemandVideoIcon />, },
+    { name: 'Gaming', icon: <SportsEsportsIcon />, },
+    { name: 'Live', icon: <LiveTvIcon />, },
+    { name: 'Sport', icon: <FitnessCenterIcon />, },
+    { name: 'Fashion', icon: <CheckroomIcon />, },
+    { name: 'Beauty', icon: <FaceRetouchingNaturalIcon />, },
+    { name: 'Comedy', icon: <TheaterComedyIcon />, },
+    { name: 'Gym', icon: <FitnessCenterIcon />, },
+    { name: 'Crypto', icon: <DeveloperModeIcon />, },
+];
+
+export const demoThumbnailUrl = 'https://ibb.co/f00gL74';
+export const demoChannelUrl = '/channel/UC-aZ1qLd07C34NFx9K8ewQQ';
+export const demoVideoUrl = '/video/aUIThMGAZY4&t';
+export const demoChannelTitle = 'Ever Show Gaming';
+export const demoVideoTitle = 'Destroyed the enemy boat | Call of Duty Mobile';
+export const demoProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png'
